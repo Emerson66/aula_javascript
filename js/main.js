51e82d2888febe7a8972a9936697eaba827bb81f
Emerson Reis <emersonreisdacosta@gmail.com>
2021-01-31 18:39:43 -0400

    aula03 - Condicionais, laços de repetição e Date

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
-var nome = "Emerson Reis";
-var idade = "29";
-var idade2 = "10";
-var frase = "Japao é o melhor time do mundo";
+var dataHoje = new Date();
+console.log(dataHoje.getMinutes());
 
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase.toUpperCase("Japao", "Brasil"));
+
+// var count;
+// for (count = 0; count <= 5; count++){
+// 	console.log(count);
+// }
+
+// while(count <= 5){
+// 	console.log(count);
+// 	count++;
+// }
+
+
+// var idade = prompt("Qual a sua idade?");
+// if(idade > 18){
+// 	console.log("maior de idade");
+// }else{
+// 	console.log("menor de idade");
+// };
+
+
+// var frutas = [{nome:"maça", cor:"vermelho"}, {nome:"uva", cor:"roxa"}];
+// console.log(frutas);
+
+// var lista = ["maça", "pera", "laranja"];
+// lista.push("uva");
+// lista.pop();
+// console.log(lista);
+// console.log(lista.toString());
+// console.log(lista.join("|"));
+
+// // var nome = "Emerson Reis";
+// // var idade = "29";
+// // var idade2 = "10";
+// // var frase = "Japao é o melhor time do mundo";
+
+// // console.log(nome);
+// // console.log(idade + idade2);
+// // console.log(frase.toUpperCase("Japao", "Brasil"));
