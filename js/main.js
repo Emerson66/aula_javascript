f08d01e658b24a303d1d1efdb74efa2463d9573c
Emerson Reis <emersonreisdacosta@gmail.com>
2021-01-31 18:03:39 -0400

    aula 01

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var nome = "Emerson Reis";
+var idade = "29";
+var idade2 = "10";
+var frase = "Japao é o melhor time do mundo";
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toUpperCase("Japao", "Brasil"));
